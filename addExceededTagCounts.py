3dd4d4a55d22140bc8e739aeb475a15f3c003d21
John Nesselt <john.nesselt@proquest.com>
2015-07-16 17:09:29 -0400

    Added email notification for index errors

--- addExceededTagCounts.py
+++ addExceededTagCounts.py
@@ -4,7 +4,11 @@
 
 import re
 import sys
+import smtplib
+import ConfigParser
+
 from os.path import expanduser
+from email.mime.text import MIMEText
 
 home = expanduser("~")
 input = sys.argv[1]
@@ -14,6 +18,9 @@ tagCounts = open(home + '/data/awsUtils/tagCounts.dat')
 instanceOwnerIn = open(input)
 instanceOwnerOut = open(output, 'w')
 
+config = ConfigParser.ConfigParser()
+config.read(home + '/data/ownerTags/ownerTags.cfg')
+
 d = {}
 for line in tagCounts:
    (key, val) = line.split()
@@ -21,13 +28,25 @@ for line in tagCounts:
 
 for line in instanceOwnerIn:
     elements = line.split()
-    if elements[6] == "None":
-        if elements[0] in d.keys():
-            instanceOwnerOut.write(line.rstrip('\n') + " - " + d[elements[0]] + " tags\n")
+
+    try:
+        if elements[6] == "None":
+            if elements[0] in d.keys():
+                instanceOwnerOut.write(line.rstrip('\n') + " - " + d[elements[0]] + " tags\n")
+            else:
+                instanceOwnerOut.write(line)
         else:
             instanceOwnerOut.write(line)
-    else:
-        instanceOwnerOut.write(line)
+    except IndexError:
+        emailFrom = config.get("OwnerTags", "EmailFrom")
+        emailTo = config.get("OwnerTags", "EmailTo")
+        msg = MIMEText("IndexError on: " + elements[0] + " from addExceededTagCounts.py")
+        msg['Subject'] = "IndexError on: " + elements[0] + " from addExceededTagCounts.py"
+        msg['From'] = emailFrom
+        msg['To'] = emailTo
+        s = smtplib.SMTP('localhost')
+        s.sendmail(emailFrom, [emailTo], msg.as_string())
+        s.quit()
 
 tagCounts.close()
 instanceOwnerIn.close()
